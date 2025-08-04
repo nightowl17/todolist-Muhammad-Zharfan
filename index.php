3943a1f418cda6bce75efb12c4b56f23706803a8
nightowl17 <81704747+nightowl17@users.noreply.github.com>
2025-08-04 15:15:03 +0700

    final changes

--- index.php
+++ index.php
@@ -55,7 +55,7 @@ if (isset($_POST['toggle_id'])) {
     <meta charset="UTF-8">
     <title>Daftar Tugas</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="style.css" rel="stylesheet">
+    <link href="assets/style.css" rel="stylesheet">
 </head>
 <body class="container py-5">
 
